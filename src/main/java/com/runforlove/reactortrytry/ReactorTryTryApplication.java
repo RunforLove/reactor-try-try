68a305e7e395a14f6b4d578916287ad12d43f0d9
sh <sh@163.com>
2019-04-03 17:12:37 +0800

    f

---
+++ src/main/java/com/runforlove/reactortrytry/ReactorTryTryApplication.java
@@ -0,0 +1,13 @@
+package com.runforlove.reactortrytry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorTryTryApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ReactorTryTryApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/runforlove/reactortrytry/ReactorTryTryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.runforlove.reactortrytry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactorTryTryApplicationTests {
+	
+	@Test
+	public void contextLoads() {
+	}
+	
+}
